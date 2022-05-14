5bfd4d125a377a6f5c2db76e3782d58b0d602d83
Lauro Obed Nolasco Vázquez <isc18350511@gmail.com>
2022-05-13 19:34:38 -0500

    Utilización de objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-//ECMAscript 5
-var textovar = "Texto var1";
-
-//ECMAScript 6
-let textolet = "Texto let 1";
-const textoconst = "Texto const 1";
-
-textovar = "texto var 2";
-textolet = "texto let 2";
-// textoconst "texto var 2";
-
-var textovar = "texto var 3";
-//let textolet = "texto let 3";
-//const textoconst = "texto const 3";
-
-if (true) {
-  var textovar = 5;
-  let textolet = 10;
-  // textoconst = 15;
-}
-
 const App = () => {
+  const miObjeto = {
+    nombre: "Lauro",
+    ApPaterno: "Nolasco",
+    ApMaterno: "Vazquez",
+    Edad: 21,
+  };
+
   return (
     <div className="App">
-      <h1>HOLA MUNDO, este es mi Segundo Proyecto</h1>
-      <h1>{textovar}</h1>
-      <h1>{textolet}</h1>
-      <h1>{textoconst}</h1>
+      {/*ECMAScript 6*/}
+      <h1>{`Mi nombre es ${miObjeto.nombre} ${miObjeto.ApPaterno} ${miObjeto.ApMaterno} y tengo ${miObjeto.Edad} años`}</h1>
+      {/*ECMAScript 6*/}
+      <h2>
+        {"Mi nombre es " +
+          miObjeto.nombre +
+          " " +
+          miObjeto.ApPaterno +
+          " " +
+          miObjeto.ApMaterno +
+          " y tengo " +
+          miObjeto.Edad +
+          " años"}
+      </h2>
     </div>
   );
 };
+
+export default App;
