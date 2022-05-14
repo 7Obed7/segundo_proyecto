aa24f4f0f69512e4c7ab84f64741d1efca0b37e3
Lauro Obed Nolasco Vázquez <isc18350511@gmail.com>
2022-05-13 19:29:16 -0500

    Ejemplo de manejo de variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+//ECMAscript 5
+var textovar = "Texto var1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//ECMAScript 6
+let textolet = "Texto let 1";
+const textoconst = "Texto const 1";
+
+textovar = "texto var 2";
+textolet = "texto let 2";
+// textoconst "texto var 2";
+
+var textovar = "texto var 3";
+//let textolet = "texto let 3";
+//const textoconst = "texto const 3";
+
+if (true) {
+  var textovar = 5;
+  let textolet = 10;
+  // textoconst = 15;
 }
 
-export default App;
+const App = () => {
+  return (
+    <div className="App">
+      <h1>HOLA MUNDO, este es mi Segundo Proyecto</h1>
+      <h1>{textovar}</h1>
+      <h1>{textolet}</h1>
+      <h1>{textoconst}</h1>
+    </div>
+  );
+};
